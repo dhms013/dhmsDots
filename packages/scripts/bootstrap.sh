522b0497203b437c45b09201825ef536912fd6ad
dhms013 <dhimas3593@gmail.com>
2026-02-16 01:51:26 +0700

    HOTFIX: change yay with yay-bin

--- packages/scripts/bootstrap.sh
+++ packages/scripts/bootstrap.sh
@@ -4,7 +4,7 @@ $PACMAN $BOOTSTRAP
 if ! command -v yay >/dev/null; then
   echo "==> Installing yay"
   rm -rf /tmp/yay
-  git clone https://aur.archlinux.org/yay.git /tmp/yay
+  git clone https://aur.archlinux.org/yay-bin.git /tmp/yay
   cd /tmp/yay
   makepkg -si --noconfirm
   cd -
