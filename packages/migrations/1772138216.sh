019660ca2c49e5fa59a6a6ed1dcab6f40bf31822
dhms013 <dhimas3593@gmail.com>
2026-02-27 09:43:46 +0700

    UPDATE: scripts, and add Omarchy repo

---
+++ packages/migrations/1772138216.sh
@@ -0,0 +1,12 @@
+echo "Add Omarchy's repo"
+
+setup_pacman() {
+  echo "==> Configuring pacman"
+  sudo mv /etc/pacman.conf /etc/pacman.conf.bak
+  sudo cp -r "$DOTFILES_DIR/packages/pacman/pacman.conf" /etc/pacman.conf
+  echo "Server = https://mirror.omarchy.org/\$repo/os/\$arch" | sudo tee -a /etc/pacman.d/mirrorlist
+}
+
+setup_pacman
+keyrings
+yay -Syyu --noconfirm --cleanafter
