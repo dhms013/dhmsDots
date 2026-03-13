2fa26f71c1dfdb2d1e579a73bbe5595a533ff0bb
dhms013 <dhimas3593@gmail.com>
2026-03-14 02:03:14 +0700

    REMOVE: starship from stow

--- packages/scripts/dotfiles.sh
+++ packages/scripts/dotfiles.sh
@@ -8,7 +8,7 @@ DOTFILES_DIR="${DOTFILES_DIR:-$HOME/.dhmsDots}"
 STOW_PKGS=(
   bash btop fastfetch ghostty
   hypr hyprland-preview-share-picker
-  kitty mako nvim rofi starship
+  kitty mako nvim rofi
   swayosd themes tmux waybar
 )
 
